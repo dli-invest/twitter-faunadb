df5081135482f11013d1a63addbb46304a372d8a
David Li <davidli012345@gmail.com>
2021-03-27 18:13:21 +0000

    Switching to flask

--- server.py
+++ server.py
@@ -1,20 +1,34 @@
-from typing import Optional
-from fastapi import FastAPI
+# from typing import Optional
+# from fastapi import FastAPI
 from get_stream import main as start_stream
 import os
-app = FastAPI()
+# app = FastAPI()
 
-# check env vars
+# # check env vars
 
-@app.get("/health-check")
+# @app.get("/health-check")
+# def read_root():
+#     return {"Hello": "World"}
+
+# @app.get("/health-check")
+# def health_check():
+#     return {"code": 200}
+
+# add endpoint to restart stream
+
+# # start_stream()
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template("index.html")
+
+@app.route("/health-check")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/health-check")
-def health_check():
-    return {"code": 200}
 
-# add endpoint to restart stream
 MANDATORY_ENV_VARS = ["DISCORD_WEBHOOK", "FAUNA_KEY", "BEARER_TOKEN"]
 
 for var in MANDATORY_ENV_VARS:
@@ -22,3 +36,4 @@ for var in MANDATORY_ENV_VARS:
         raise EnvironmentError("Failed because {} is not set.".format(var))
 
 start_stream()
+app.run(host='0.0.0.0', port=8080)
